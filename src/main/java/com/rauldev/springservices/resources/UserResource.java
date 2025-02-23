84ce68da5ada8d71fcb9a076f90498ee67d63164
RaulRodriguesss <santos.raulrodr@gmail.com>
2025-02-23 13:20:19 -0300

    creation User and resource

---
+++ src/main/java/com/rauldev/springservices/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.rauldev.springservices.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rauldev.springservices.entities.User;
+
+@RestController
+@RequestMapping(value="/Users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		
+		
+		User u = new User(1L,"maria","maria@gmail.com","38988112233","lokura");
+		return ResponseEntity.ok().body(u);
+	}
+}
